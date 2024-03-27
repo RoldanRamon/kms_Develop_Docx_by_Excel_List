dd9c302680dcab024f81204566dff01cda15108d
Ramon <roldanramon83@gmail.com>
2024-03-27 17:09:54 -0300

    update

--- ETL.R
+++ ETL.R
@@ -13,21 +13,41 @@ base <- readxl::read_excel('./Arquivos/APP BOLETINS TECNICOS.xlsx',sheet = 'base
   mutate(nome_do_arquivo = paste0(codigo,'_',modelos,'_') %>% stringr::str_replace_all(pattern = ' ',replacement = '_'))
 
 
-# Função para criar um documento DOCX com uma linha como título
-criar_docx <- function(row) {
+# Função para criar um documento DOCX com uma linha como título e adicionar uma imagem
+criar_docx <- function(row, codigo) {
   doc <- read_docx()
   doc <- doc %>%
-    body_add_par(row, style = "heading 1")
+    body_add_par(row, style = "heading 1") 
+  
+  caminho <- case_when(codigo == "fo" ~ "Imagens/Forwarder.jpeg",
+                       codigo == "ha" ~ "Imagens/Harvester.JPG",
+                       codigo == "pc" ~ "Imagens/PC_200.jpg",
+                       codigo == "ca" ~ "Imagens/Cabecote.jpg",
+                       codigo == "ll" ~ "Imagens/LogLoader.jpg",
+                       codigo == "cs" ~ "Imagens/Guincho-Komatsu.jpg",
+                       codigo == "tp" ~ "Imagens/TimberPro.png",
+                       codigo == "fe" ~ "Imagens/TimberPro.png",
+                       codigo == "si" ~ "Imagens/Silvicultura.png",
+                       TRUE ~ ""
+                       )
+  
+  if (caminho != "") {
+    doc <- doc %>% 
+      body_add_img(src = caminho, width = 8, height = 8)
+  }
+  
+  
   return(doc)
 }
-
 
 # Mapear nomes de modelo diretamente
 nomes_modelos <- base$nome_do_arquivo
 
 # Iterar sobre cada linha e criar um documento DOCX para cada modelo
-docs <- purrr::map(nomes_modelos, criar_docx)
+docs <- purrr::map2(nomes_modelos, base$codigo, criar_docx)
 
 # Salvar cada documento em um arquivo separado com o nome do modelo
-walk2(docs, nomes_modelos, ~print(.x, target = paste0("Exemplo de Boletins/", .y, "Restante do Titulo.docx")))
+walk2(docs, nomes_modelos, ~print(.x, target = paste0("Exemplo de Boletins/", .y, "Exemplo_01.docx")))
+walk2(docs, nomes_modelos, ~print(.x, target = paste0("Exemplo de Boletins/", .y, "Exemplo_02.docx")))
+walk2(docs, nomes_modelos, ~print(.x, target = paste0("Exemplo de Boletins/", .y, "Exemplo_03.docx")))
 
